da71e6baeea233f78792f9fcf5e9ec0f5384e42c
ud <udityarajpandey48@gmail.com>
2026-08-09 17:31:29 +0530

    ok2

--- app.py
+++ app.py
@@ -4,17 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("/lemplates/index.html")
+    return render_template("index.html")
 
 @app.route("/login")
 def login():
-    return render_template("/lemplates/login.html")
-
+    return render_template("login.html")
 
 @app.route("/register")
 def register():
-    return render_template("/lemplates/register.html")
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    return render_template("register.html")
